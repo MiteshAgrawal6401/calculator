4f9ef22f05d2fe7404e0be198e7eb73263e751b8
Mitesh <miteshagrawal6401@gmail.com>
2023-03-29 00:34:06 +0530

    javascript functions

--- calculator.js
+++ calculator.js
@@ -14,19 +14,55 @@ let divide=function(a,b){
     return a/b;
 }
 
+let screen_btn=document.querySelector('screen');
+
+
+let result;
+
+
+//operate function
 let operate=function(a,b,operator){
 
     if(operator == '+'){
-        return a+b;
+        result = a+b;
+        return result;
     }
 
     if(operator == '-'){
-        return a-b;
+        result = a-b;
+        return result;
     }
     if(operator == '*'){
-        return a*b;
+        result = a*b;
+        return result;
     }
     if(operator == '/'){
-        return a/b;
+        result = a/b;
+        return result;
     }
 }
+
+//button
+let button_num=document.querySelector('number');
+button_num.addEventListener("click",function(e){
+    let value=number.value;
+    screen_btn.value+=value;
+})
+
+
+
+//clear
+let clear_display=document.querySelector('clear');
+clear_display.addEventListener("click",location.reload());
+
+//equal
+let diaplay_ans=document.querySelector('equal');
+diaplay_ans.addEventListener("click",function(e){
+    screen_btn.value+=result;
+})
+
+
+   
+
+    
+   
